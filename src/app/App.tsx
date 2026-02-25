5cb095d8590a753d18b7fc147423d548c7cb4897
Zubair <92728512+zubairameer@users.noreply.github.com>
2026-02-25 15:43:52 +0200

    Update App.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 import { motion } from 'motion/react';
 
 export default function App() {
-  const text = "12345789987654321  •  abcdefgefhgdrtckiu  •  !@#$%^&*((*&^%$#@!  •  ";
+  const text = "Hints of cedarwood  •  Soft jasmine  •  Delicate rose  •  Touch of warm vanilla  •  Whisper of sandalwood  •";
   const [characters, setCharacters] = useState<string[]>([]);
 
   useEffect(() => {
